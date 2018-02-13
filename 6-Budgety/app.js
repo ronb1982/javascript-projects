9df5030e58e7e3e3ef89c87c7b7d8c0c2ddac931
ronb <ronbnj@gmail.com>
2018-02-13 17:58:37 -0500

    Added Regex for thousands places in numbers

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -168,9 +168,12 @@ var UIController = (function() {
 
         int = numSplit[0];
 
-        if (int.length > 3) {
+        /*if (int.length > 3) {
             int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
-        }
+        }*/
+
+        // Use Regex for comma places
+        int = int.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
         dec = numSplit[1];
 
